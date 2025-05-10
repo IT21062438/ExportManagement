565c5eeef44c4fa7c7f264077269a33bea71afd7
Harendra Samarawickrama <it21062438@my.sliit.lk>
2025-05-10 19:05:21 +0530

    Update InquiryReports.js

--- client/src/components/InquiryReports.js
+++ client/src/components/InquiryReports.js
@@ -1,66 +1,81 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import jsPDF from 'jspdf'
-import 'jspdf-autotable'
-import ReactHtmlTableToExcel from 'react-html-table-to-excel'
+import React, { Component } from "react";
+import axios from "axios";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import ReactHtmlTableToExcel from "react-html-table-to-excel";
 
-const generatePDF =inquiries =>{
-  const doc =new jsPDF();
-  const tableColumn =["Inquiry ID", "Date", "Customer Name", "Contact No", "Email", "Inquiry Type" , "Description"];
-  const tableRows=[];
+const generatePDF = (inquiries) => {
+  const doc = new jsPDF();
+  const tableColumn = [
+    "Inquiry ID",
+    "Date",
+    "Customer Name",
+    "Contact No",
+    "Email",
+    "Inquiry Type",
+    "Description",
+  ];
+  const tableRows = [];
 
-  inquiries.map(inquiries=>{
-    const inquiryData=[
-      inquiries.inquiryID,
-      inquiries.date,
-      inquiries.customerName,
-      inquiries.contactNo,
-      inquiries.email,
-      inquiries.inquiryType,
-      inquiries.description,
+  inquiries.map((inquiry) => {
+    const formattedDate = (() => {
+      const date = new Date(inquiry.date);
+      const dd = String(date.getDate()).padStart(2, "0");
+      const mm = String(date.getMonth() + 1).padStart(2, "0");
+      const yyyy = date.getFullYear();
+      return `${dd}/${mm}/${yyyy}`;
+    })();
+
+    const inquiryData = [
+      inquiry.inquiryID,
+      formattedDate,
+      inquiry.customerName,
+      inquiry.contactNo,
+      inquiry.email,
+      inquiry.inquiryType,
+      inquiry.description,
     ];
 
     tableRows.push(inquiryData);
-  })
-    doc.text("Ceylon Exports", 70,8).setFontSize(13);
-    doc.text("CUSTOMER INQUIRY SUMMARY", 14, 16).setFontSize(13); 
-    doc.autoTable(tableColumn, tableRows, { styles: { fontSize: 8, }, startY: 35 });
-    doc.save("InquirySummary.pdf");
-
-
-}
+  });
+  doc.text("Ceylon Exports", 70, 8).setFontSize(13);
+  doc.text("CUSTOMER INQUIRY SUMMARY", 14, 16).setFontSize(13);
+  doc.autoTable(tableColumn, tableRows, {
+    styles: { fontSize: 8 },
+    startY: 35,
+  });
+  doc.save("InquirySummary.pdf");
+};
 
 export default class InquiryReports extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state={
-      inquiries:[]
+    this.state = {
+      inquiries: [],
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.retrieveInquiries();
   }
 
-  retrieveInquiries(){
-    axios.get("/inquiries").then(res =>{
+  retrieveInquiries() {
+    axios.get("/inquiries").then((res) => {
       if (res.data.success) {
         this.setState({
-          inquiries:res.data.existingInquiries
+          inquiries: res.data.existingInquiries,
         });
 
-        console.log(this.state.inquiries)
+        console.log(this.state.inquiries);
       }
     });
   }
 
-
   render() {
     return (
-      <div id='wrapper' className='toggled'>
-         <nav
+      <div id="wrapper" className="toggled">
+        <nav
           class="navbar navbar-expand-lg rounded-3"
           style={{ backgroundColor: "#006a4e", marginTop: "5px" }}
         >
@@ -104,46 +119,43 @@ export default class InquiryReports extends Component {
         <hr />
 
         <h4>All Customer Inquiries</h4>
-      <div id="page-content-wrapper">
-
-      <ReactHtmlTableToExcel
-        id="test-table-xls-button"
-        className="btn btn-info"
-        table="tableee"
-        filename="InquirySummary"
-        sheet="tablexls"
-        buttonText="Download as Excel"
-      />
-      &nbsp;
-
-      <button 
-      type='button'
-      style={{backgroundColor:"#00000", padding: "7px"}}
-      className="btn btn-info btn-sm"
-      onClick={()=> generatePDF(this.state.inquiries)}
-      >
-        Download As PDF
-      </button>
-
-      <table id="tableee" className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Inquiry ID</th>
-              <th scope="col">Date</th>
-              <th scope="col">Customer Name</th>
-              <th scope="col">Contact No</th>
-              <th scope="col">Email</th>
-              <th scope="col">Inquiry Type</th>
-              <th scope="col">Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.inquiries.map((inquiries,index)=>(
-              <tr key={index}>
-                <th scope="row">{index+1}</th>
-                <td>{inquiries.inquiryID}</td>
-                <td>
+        <div id="page-content-wrapper">
+          <ReactHtmlTableToExcel
+            id="test-table-xls-button"
+            className="btn btn-info"
+            table="tableee"
+            filename="InquirySummary"
+            sheet="tablexls"
+            buttonText="Download as Excel"
+          />
+          &nbsp;
+          <button
+            type="button"
+            style={{ backgroundColor: "#00000", padding: "7px" }}
+            className="btn btn-info btn-sm"
+            onClick={() => generatePDF(this.state.inquiries)}
+          >
+            Download As PDF
+          </button>
+          <table id="tableee" className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Inquiry ID</th>
+                <th scope="col">Date</th>
+                <th scope="col">Customer Name</th>
+                <th scope="col">Contact No</th>
+                <th scope="col">Email</th>
+                <th scope="col">Inquiry Type</th>
+                <th scope="col">Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.inquiries.map((inquiries, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{inquiries.inquiryID}</td>
+                  <td>
                     {(() => {
                       const date = new Date(inquiries.date);
                       const dd = String(date.getDate()).padStart(2, "0");
@@ -152,21 +164,17 @@ export default class InquiryReports extends Component {
                       return `${dd}/${mm}/${yyyy}`;
                     })()}
                   </td>
-                <td>{inquiries.customerName}</td>
-                <td>{inquiries.contactNo}</td>
-                <td>{inquiries.email}</td>
-                <td>{inquiries.inquiryType}</td>
-                <td>{inquiries.description}</td>
-      
-              </tr>
-            ))}
-          </tbody>  
-        </table>
-
-
+                  <td>{inquiries.customerName}</td>
+                  <td>{inquiries.contactNo}</td>
+                  <td>{inquiries.email}</td>
+                  <td>{inquiries.inquiryType}</td>
+                  <td>{inquiries.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      </div>
-
-    )
+    );
   }
 }
